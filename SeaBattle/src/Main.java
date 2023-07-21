d50a82f856b5a959cc354c0fdc17b28a9afb7149
Evelina Kunsmanaite <evelinakunsmanaite@gmail.com>
2023-07-21 16:06:31 +0300

    add new changes

--- SeaBattle/src/Main.java
+++ SeaBattle/src/Main.java
@@ -2,31 +2,48 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] playingField = new int[10][10];
+        String[][] playingField = new String[10][10];
+        int count = 0;
         for (int i = 0; i < playingField.length; i++) {
             for (int j = 0; j < playingField[i].length; j++) {
-                if (playingField[i][j] == 0) {
-                    System.out.print("\uD83D\uDFE5");
-                }
+                playingField[i][j] = "\u2B1C";
+                System.out.print(playingField[i][j] + "  ");
             }
             System.out.println("");
         }
+        while (count < 4) {
+            rec(playingField);
+            count++;
+        }
+    }
+
+    public static int rec(String[][] playingField) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите координату однопалубного корабля");
         String[] coordinates = scanner.next().split(",");
         int x = Integer.parseInt(coordinates[0]);
         int y = Integer.parseInt(coordinates[1]);
-        System.out.println(x);
-        System.out.println(y);
+
         for (int i = 0; i < playingField.length; i++) {
             for (int j = 0; j < playingField[i].length; j++) {
- if (i == x && j == y) {
-                    System.out.print("\uD83D\uDEE5");
-                } else                 if (playingField[i][j] == 0) {
-     System.out.print("\uD83D\uDFE5");
- }
+                if (i == x && j == y) {
+                    if (playingField[x][y].contains("\u2B1C")) {
+                        playingField[x][y] = "\uD83D\uDEE5";
+                    } else if (!playingField[x][y].contains("\u2B1C")) {
+                        System.out.println("Error");
+                        return rec(playingField);
+                    }
+                }
+            }
+
+        }
+        for (String[] strings : playingField) {
+            for (String string : strings) {
+                System.out.print(string + "  ");
             }
             System.out.println("");
         }
+        return 0;
     }
 }
